c4589dc65611509493a867fd3b4c0f776930f75c
ahmadrymera <ahmad@rymera.com.au>
2023-02-24 06:15:26 +0700

    adding automation test

--- cypress/integration/login.ts
+++ cypress/integration/login.ts
@@ -5,7 +5,7 @@ let loginPage = new LoginPage()
 let dashboardPage = new DashboardPage()
 const URL = 'https://www.saucedemo.com/'
 
-it('Test LOGIN', () => {
+it('Success login with correct credential', () => {
     loginPage.login(URL,'standard_user','secret_sauce')
     loginPage.assertLogin() 
 })
@@ -18,5 +18,30 @@ it('Test Sauce Demo Invalid Password', () => {
 it('Test Sauce Demo Sauce labs product backpack', () => {
     loginPage.login(URL,'standard_user','secret_sauce')
     loginPage.assertLogin()
-    dashboardPage.sauceLabsBackpack() 
+    dashboardPage.openProductPage('Sauce Labs Backpack') 
+})
+
+it('Success add product to cart from shop page', ()=>{
+    loginPage.login(URL,'standard_user','secret_sauce')
+    loginPage.assertLogin()
+    dashboardPage.addProductFromShopPage('Sauce Labs Onesie')
+    dashboardPage.checkCartValueShouldBe('1')
+})
+
+it('Success remove product to cart from shop page', ()=>{
+    dashboardPage.removeCurrentAddedProductFromShopPage('Sauce Labs Onesie')
+    dashboardPage.checkAddButtonProductOnTheShopPage('Sauce Labs Onesie', 'Add to cart')
+})
+
+it('Success add product from product page', ()=>{
+    loginPage.login(URL,'standard_user','secret_sauce')
+    loginPage.assertLogin()
+    dashboardPage.openProductPage('Sauce Labs Onesie')
+    dashboardPage.addProductFromProductPage('Sauce Labs Onesie')
+    dashboardPage.checkCartValueShouldBe('1')
+})
+
+it('Success remove product to cart from product page', ()=>{
+    dashboardPage.removeProductFromProductPage('Sauce Labs Onesie')
+    dashboardPage.checkAddButtonProductOnTheProductPage('Sauce Labs Onesie', 'Add to cart')
 })
--- cypress/integration/pages/dashboard_pages.ts
+++ cypress/integration/pages/dashboard_pages.ts
@@ -1,8 +1,68 @@
-export class DashboardPage{
-    link_sauceLabsBackpack = 'Sauce Labs Backpack'
+export class DashboardPage {
+  slugify = (str: string) =>
+    str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, "")
+      .replace(/[\s_-]+/g, "-")
+      .replace(/^-+|-+$/g, "");
 
-    sauceLabsBackpack(){
-        cy.contains(this.link_sauceLabsBackpack).click()
-        cy.contains('Sauce Labs Backpack').should('be.visible') 
-    }
+  openProductPage(productName: string) {
+    cy.contains(productName).click();
+    cy.contains(productName).should("be.visible");
+  }
+
+  addProductFromShopPage(productName: string) {
+    cy.get(".inventory_item_description")
+      .contains(productName)
+      .parentsUntil(".inventory_item")
+      .find(`#add-to-cart-${this.slugify(productName)}`)
+      .should("contain.text", "Add to cart")
+      .click();
+  }
+
+  removeCurrentAddedProductFromShopPage(productName: string) {
+    cy.get(".inventory_item_description")
+      .contains(productName)
+      .parentsUntil(".inventory_item")
+      .find(`#remove-${this.slugify(productName)}`);
+    cy.contains("Remove").click();
+  }
+
+  addProductFromProductPage(productName: string) {
+    cy.get(".inventory_details_name")
+      .contains(productName)
+      .nextAll(`#add-to-cart-${this.slugify(productName)}`)
+      .should("contain.text", "Add to cart")
+      .click();
+  }
+
+  removeProductFromProductPage(productName: string) {
+    cy.get(".inventory_details_name")
+      .contains(productName)
+      .nextAll(`#remove-${this.slugify(productName)}`)
+      .should("contain.text", "Remove")
+      .click();
+  }
+
+  checkAddButtonProductOnTheShopPage(productName: string, status: string) {
+    cy.get(".inventory_item_description")
+      .contains(productName)
+      .parentsUntil(".inventory_item")
+      .find(`#${this.slugify(status)}-${this.slugify(productName)}`)
+      .should("contain.text", status);
+  }
+
+  checkAddButtonProductOnTheProductPage(productName: string, status: string) {
+    cy.get(".inventory_details_name")
+      .contains(productName)
+      .nextAll(`#${this.slugify(status)}-${this.slugify(productName)}`)
+      .should("contain.text", status);
+  }
+
+  checkCartValueShouldBe(item: string) {
+    cy.get(
+      "#shopping_cart_container > .shopping_cart_link > .shopping_cart_badge"
+    ).should("have.text", item);
+  }
 }
